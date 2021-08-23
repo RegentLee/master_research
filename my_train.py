c27589c6f26960cbbd1022d1ecc593cd55276ea2
Mingyeh Lee <regentlee.com@gmail.com>
2021-08-23 14:41:34 +0900

    fix bug

--- my_train.py
+++ my_train.py
@@ -47,7 +47,11 @@ if __name__ == '__main__':
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
         visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
         # model.train()
-        opt.isTrain = True
+        # opt.isTrain = True
+        for name in model.model_names:
+            if isinstance(name, str):
+                net = getattr(model, 'net' + name)
+                net.eval()
         model.update_learning_rate()    # update learning rates in the beginning of every epoch.
         for i, data in enumerate(dataset):  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
