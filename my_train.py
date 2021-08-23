21647dc8efd3fdbff145e1b880aec20a0b41f192
Mingyeh Lee <regentlee.com@gmail.com>
2021-08-23 14:37:31 +0900

    fix bug

--- my_train.py
+++ my_train.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     print('The number of training images = %d' % dataset_size)
     opt._val = True
     valset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-    valset_size = len(dataset)    # get the number of images in the dataset.
+    valset_size = len(valset)    # get the number of images in the dataset.
     print('The number of validation images = %d' % valset_size)
 
     model = create_model(opt)      # create a model given opt.model and other options
@@ -46,7 +46,8 @@ if __name__ == '__main__':
         iter_data_time = time.time()    # timer for data loading per iteration
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
         visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
-        model.train()
+        # model.train()
+        opt.isTrain = True
         model.update_learning_rate()    # update learning rates in the beginning of every epoch.
         for i, data in enumerate(dataset):  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
