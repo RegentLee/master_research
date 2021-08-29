7cfe767c28d73052d4f40900bd55e338081b229b
Mingyeh Lee <regentlee.com@gmail.com>
2021-08-29 23:34:41 +0900

    Revert "add symmetry in fake_A and fake_B"
    This reverts commit 806e3412ddce9c44679e6f9b1e0a2e5be64789cd.

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -112,16 +112,8 @@ class CycleGANModel(BaseModel):
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         self.fake_B = self.netG_A(self.real_A)  # G_A(A)
-        for i in range(len(self.fake_B)):
-            for j in range(i+1, len(self.fake_B)):
-                self.fake_B[i][j] = (self.fake_B[i][j] + self.fake_B[j][i])/2
-                self.fake_B[j][i] = self.fake_B[i][j]
         self.rec_A = self.netG_B(self.fake_B)   # G_B(G_A(A))
         self.fake_A = self.netG_B(self.real_B)  # G_B(B)
-        for i in range(len(self.fake_A)):
-            for j in range(i+1, len(self.fake_A)):
-                self.fake_A[i][j] = (self.fake_A[i][j] + self.fake_A[j][i])/2
-                self.fake_A[j][i] = self.fake_A[i][j]
         self.rec_B = self.netG_A(self.fake_A)   # G_A(G_B(B))
 
     def backward_D_basic(self, netD, real, fake):
